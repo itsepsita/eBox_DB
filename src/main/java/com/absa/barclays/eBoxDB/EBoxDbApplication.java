e00bd8be79d82ec18f34f45f0329739ddf9c944a
itsepsita <77191477+itsepsita@users.noreply.github.com>
2025-02-25 14:37:29 +0530

    Add files via upload

---
+++ src/main/java/com/absa/barclays/eBoxDB/EBoxDbApplication.java
@@ -0,0 +1,13 @@
+package com.absa.barclays.eBoxDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBoxDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBoxDbApplication.class, args);
+	}
+
+}
